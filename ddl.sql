42c401c01df3db2d195933f9dc928080a5a89b09
joshuajohn08 <kjoshjohn@gmail.com>
2024-04-11 12:19:41 -0400

    Created Members table in ddl file

--- ddl.sql
+++ ddl.sql
@@ -11,3 +11,24 @@ CREATE TABLE Members (
     fitness_goal VARCHAR(255)
     join_date DATE DEFAULT CURRENT_DATE
 );
+
+CREATE TABLE Trainers (
+    trainer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) UNIQUE,
+    date_of_birth DATE
+)
+
+CREATE TABLE Administration (
+    trainer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) UNIQUE,
+    date_of_birth DATE
+)
+
