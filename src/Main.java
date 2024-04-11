bfd9cd7e1ef41a7577ae3262bc2cd2aa25a65619
Aekus Trehan <aekus.trehan@gmail.com>
2024-04-11 11:40:11 -0400

    Added basic program loop for initial sign in and user registration

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, SQLException {
@@ -15,5 +16,28 @@ public class Main {
 
         Connection conn = DriverManager.getConnection(prop.getProperty("DB_URL"), prop.getProperty("DB_USER"), prop.getProperty("DB_PASSWORD"));
         System.out.println("Connected");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(true) {
+            System.out.println("Welcome to the Fitness Club");
+            System.out.println("1. Sign in as existing user");
+            System.out.println("2. Register new user");
+            System.out.println("Enter your choice: ");
+
+            String choice = scanner.nextLine();
+
+            switch (choice) {
+                case "1":
+                    System.out.println("sign in");
+                    break;
+                case "2":
+                    System.out.println("Register new user");
+                    break;
+                default:
+                    System.out.println("Invalid choice\n");
+                    break;
+            }
+        }
     }
 }
