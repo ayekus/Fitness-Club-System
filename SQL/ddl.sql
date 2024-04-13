ad30229a3afb7af4126241d786c1974b0611de53
joshuajohn08 <kjoshjohn@gmail.com>
2024-04-13 19:25:05 -0400

    Made a SQL folder to store dml and ddl

---
+++ SQL/ddl.sql
@@ -0,0 +1,103 @@
+CREATE TABLE Members (
+    member_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) UNIQUE,
+    date_of_birth DATE,
+    height DECIMAL(5, 2),
+    weight DECIMAL(5, 2),
+    fitness_goal VARCHAR(255),
+    exercise_routines VARCHAR(255),
+    fitness_achievements VARCHAR(255),
+    health_stats VARCHAR(255)
+);
+
+CREATE TABLE Trainers (
+    trainer_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) UNIQUE,
+    date_of_birth DATE
+);
+
+CREATE TABLE TrainerAvailability (
+    availability_id SERIAL PRIMARY KEY,
+    trainer_id INT NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    is_group_availability BOOLEAN NOT NULL,
+    session_name VARCHAR(255),
+    FOREIGN KEY (trainer_id) REFERENCES Trainers(trainer_id)
+);
+
+
+CREATE TABLE Admin (
+    admin_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(20) UNIQUE,
+    date_of_birth DATE
+);
+
+CREATE TABLE EquipmentMaintenance (
+    maintenance_id SERIAL PRIMARY KEY,
+    equipment_name VARCHAR(255) NOT NULL,
+    admin_id INT NOT NULL,
+    date_added DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (admin_id) REFERENCES Admin(admin_id)
+);
+
+CREATE TABLE TrainingSession (
+    training_session_id SERIAL PRIMARY KEY,
+    member_id INTEGER NOT NULL,
+    trainer_id INTEGER NOT NULL,
+    session_date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    session_name VARCHAR(255) NOT NULL,
+    FOREIGN KEY (member_id) REFERENCES Members(member_id),
+    FOREIGN KEY (trainer_id) REFERENCES Trainers(trainer_id)
+);
+
+CREATE TABLE GroupSession (
+    group_session_id SERIAL PRIMARY KEY,
+    session_name VARCHAR(255) NOT NULL,
+    session_date DATE NOT NULL,
+    start_time TIME NOT NULL,
+    end_time TIME NOT NULL,
+    trainer_id INTEGER NOT NULL,
+    FOREIGN KEY (trainer_id) REFERENCES Trainers(trainer_id)
+);
+
+CREATE TABLE GroupSessionEnrollment (
+    enrollment_id SERIAL PRIMARY KEY,
+    group_session_id INTEGER NOT NULL,
+    member_id INTEGER NOT NULL,
+    FOREIGN KEY (group_session_id) REFERENCES GroupSession(group_session_id),
+    FOREIGN KEY (member_id) REFERENCES Members(member_id)
+);
+
+CREATE TABLE Payments (
+    payment_id SERIAL PRIMARY KEY,
+    member_id INTEGER NOT NULL,
+    amount DECIMAL(10, 2) NOT NULL,
+    payment_desc VARCHAR(255),
+    payment_date DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (member_id) REFERENCES Members(member_id)
+);
+
+CREATE TABLE ApprovedPayments (
+    approved_payment_id SERIAL PRIMARY KEY,
+    member_id INTEGER NOT NULL,
+    payment_date DATE NOT NULL,
+    admin_id INTEGER NOT NULL,
+    payment_desc VARCHAR(255),
+    date_approved DATE DEFAULT CURRENT_DATE,
+    FOREIGN KEY (admin_id) REFERENCES Admin(admin_id),
+);
